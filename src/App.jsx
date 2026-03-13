bd39354da1cf1dc4959e50a1f2470401c2abe7ff
Adeniyi Olusegun Kugbiyi <kugbiyiadeniyi@gmail.com>
2026-03-13 14:03:02 +0000

    Preceding changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import WSFManagement from "@/pages/WSFManagement";
 import UserManagement from "@/pages/UserManagement";
 import Auth from "@/pages/Auth";
 import ResetPassword from "@/pages/ResetPassword";
+import MyProfile from "@/pages/MyProfile";
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
