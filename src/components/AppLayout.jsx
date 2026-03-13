e65098ad070e346803f72b542fc944740e1c8b85
Adeniyi Olusegun Kugbiyi <kugbiyiadeniyi@gmail.com>
2026-03-13 18:15:57 +0000

    Preceding changes

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -20,7 +20,7 @@ const allNavItems = [
   { name: "Communications", icon: Megaphone, path: "/communications", access: "leader" },
   { name: "Transportation", icon: Car, path: "/transportation", access: null },
   { name: "Analytics", icon: BarChart2, path: "/analytics", access: "leader" },
-  { name: "WSF Centres", icon: Globe, path: "/wsf", access: "admin" },
+  { name: "WSF Centres", icon: Globe, path: "/wsf", access: "wsf" },
   { name: "User Management", icon: Shield, path: "/user-management", access: "admin" },
   { name: "Audit Log", icon: FileText, path: "/audit-log", access: "super_admin" },
 ];
