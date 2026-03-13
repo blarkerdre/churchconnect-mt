f804c703b4f0f16c962381daededb04a26b8eb5f
Adeniyi Olusegun Kugbiyi <kugbiyiadeniyi@gmail.com>
2026-03-13 18:12:21 +0000

    Preceding changes

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -19,6 +19,7 @@ export function AuthProvider({ children }) {
         } else {
           setProfile(null);
           setRoles([]);
+          setLeaderUnits([]);
           setLoading(false);
         }
       }
