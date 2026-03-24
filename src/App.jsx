e0a22b36c12a96473c9c8d5dd43b06c436fa6d46
Adeniyi Olusegun Kugbiyi <kugbiyiadeniyi@gmail.com>
2026-03-24 18:16:28 +0000

    Preceding changes

--- src/App.jsx
+++ src/App.jsx
@@ -137,9 +137,11 @@ function AuthRoutes() {
         element={
           <ProtectedRoute>
             <TenantProvider>
-              <Layout>
-                <AppPages />
-              </Layout>
+              <TenantThemeProvider>
+                <Layout>
+                  <AppPages />
+                </Layout>
+              </TenantThemeProvider>
             </TenantProvider>
           </ProtectedRoute>
         }
